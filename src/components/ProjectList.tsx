6e7968f7339b6180347e22df5bc88b25bf3f0ba2
joellekado <jkado@my.hpu.edu>
2022-05-01 18:12:09 -0500

    developed ProjectList with function component and hooks

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -1,7 +1,56 @@
+import { useState, useEffect } from "react";
+
 const ProjectList = () => {
+const [newId, setNewId] = useState('');
+const [newProjectName, setNewProjectName]  = useState('')
+const [newIsActive, setNewIsActive] = useState(true);   
+
+const newProject = {
+        id: newId,
+        projectName: newProjectName,
+        isActive: newIsActive,
+    },
+    projects = [
+        {
+            id: '1',
+            projectName: 'Demo Project',
+            isActive: true,
+        }
+    ]
+
     return (
         <>
             <h2>Function Compoent with Hooks and Redux</h2>
+            <div style={{ padding: 20 }}>
+                    ID: <input value={newId} onChange={(e) => setNewId( e.target.value)} />
+                    <br />
+                    Name: <input value={newProjectName} onChange={(e) => setNewProjectName( e.target.value)} />
+                    <br />
+                    Active:
+                    <select name="active" id="active" value={newIsActive} onChange={(e) => setNewIsActive( e.target.value)}>
+                        <option value="true">Yes</option>
+                        <option value="false">No</option>
+                    </select>
+                    <button onClick={() => {
+                        projects.push(newProject)
+                    setNewId('')
+                    setNewProjectName('')
+                    setNewIsActive(true)
+
+                    
+                    }}>
+                        Submit
+                    </button>
+                </div>
+
+                {
+                    projects.map(project => (
+                        <div style={{borderStyle: 'solid', margin: 10 }}>
+                            <p>ID: {project.id}</p>
+                            <p>Name: {project.projectName}</p>
+                        </div>
+                    ))
+                }     
         </>
     );
 }
